09035fd490e99d944d8b833813ae6ebf78daf587
jcdourado <jcd17071997@hotmail.com>
2015-09-09 08:00:44 -0300

    Teste commit secundario

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -3,7 +3,7 @@ package view;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Teste commit inicial");
+		System.out.println("Teste commit secundario");
 	}
 
 }
