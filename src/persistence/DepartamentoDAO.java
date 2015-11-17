6a14ae68b1df32aace65ab6b10640cf595b1ac7f
jcdourado <jcd17071997@hotmail.com>
2015-11-17 17:45:13 -0200

    Coloquei o if para montar a string sql

--- src/persistence/DepartamentoDAO.java
+++ src/persistence/DepartamentoDAO.java
@@ -86,57 +86,66 @@ public class DepartamentoDAO {
 		ps.close();
 		return lista;
 	}
-
-	public List<Departamento> consPorId(int id) throws SQLException {
-		List<Departamento> lista = new ArrayList<Departamento>();
-		String sql = "SELECT codDepartamento, nome, andar, predio, telefone,"
-				+ " codResponsavel FROM departamento WHERE codDepartamento LIKE '%?%'";
-		PreparedStatement ps = c.prepareStatement(sql);
-		ps.setInt(1, id);
-		ResultSet rs = ps.executeQuery();
-		while(rs.next()){
-			Departamento dp = new Departamento();
-			dp.setId(rs.getInt("codDepartamento"));
-			dp.setNome(rs.getString("nome"));
-			dp.setAndar(rs.getString("andar"));
-			dp.setPredio(rs.getString("predio"));
-			dp.setTelefone(rs.getString("telefone"));
-			dp.setCodResponsavel(rs.getInt("codResponsavel"));
-			lista.add(dp);
-		}
-		rs.close();
-		ps.close();
-		return lista;
-	}
-
-	public List<Departamento> consPorNome(String nome) throws SQLException {
-		List<Departamento> lista = new ArrayList<Departamento>();
-		String sql = "SELECT codDepartamento, nome, andar, predio, telefone,"
-				+ " codResponsavel FROM departamento WHERE nome LIKE '%?%'";
-		PreparedStatement ps = c.prepareStatement(sql);
-		ps.setString(1, nome);
-		ResultSet rs = ps.executeQuery();
-		while(rs.next()){
-			Departamento dp = new Departamento();
-			dp.setId(rs.getInt("codDepartamento"));
-			dp.setNome(rs.getString("nome"));
-			dp.setAndar(rs.getString("andar"));
-			dp.setPredio(rs.getString("predio"));
-			dp.setTelefone(rs.getString("telefone"));
-			dp.setCodResponsavel(rs.getInt("codResponsavel"));
-			lista.add(dp);
-		}
-		rs.close();
-		ps.close();
-		return lista;
-	}
 	
-	public List<Departamento> consPorNome(String nome) throws SQLException {
-		List<Departamento> lista = new ArrayList<Departamento>();
+	private String getSql(Departamento d){
+		int ver = 0;
 		String sql = "SELECT codDepartamento, nome, andar, predio, telefone,"
-				+ " codResponsavel FROM departamento WHERE nome LIKE '%?%'";
-		PreparedStatement ps = c.prepareStatement(sql);
-		ps.setString(1, nome);
+				+ " codResponsavel FROM departamento ";
+		if(d.getId() != 0 ){
+			sql += "WHERE codDepartamento LIKE '%" +d.getId()+"%' ";
+			ver++;
+		}		
+		if(d.getNome() != null){
+			if(ver>0){
+				sql += "AND nome LIKE '%"+d.getNome()+"%' ";
+			}
+			else{
+				sql += "WHERE nome LIKE '%"+d.getNome()+"%' ";	
+				ver++;
+			}
+		}		
+		if(d.getAndar() != null){
+			if(ver>0){
+				sql += "AND andar LIKE '%"+d.getAndar()+"%' ";
+			}
+			else{
+				sql += "WHERE andar LIKE '%"+d.getAndar()+"%' ";	
+				ver++;
+			}
+		}
+		if(d.getPredio() != null){
+			if(ver>0){
+				sql += "AND predio LIKE '%"+d.getPredio()+"%' ";
+			}
+			else{
+				sql += "WHERE predio LIKE '%"+d.getPredio()+"%' ";	
+				ver++;
+			}
+		}
+		if(d.getTelefone() != null){
+			if(ver>0){
+				sql += "AND telefone LIKE '%"+d.getTelefone()+"%' ";
+			}
+			else{
+				sql += "WHERE telefone LIKE '%"+d.getTelefone()+"%' ";	
+				ver++;
+			}
+		}
+		if(d.getCodResponsavel() != 0){
+			if(ver>0){
+				sql += "AND codResponsavel LIKE '%"+d.getCodResponsavel()+"%' ";
+			}
+			else{
+				sql += "WHERE codResponsavel LIKE '%"+d.getCodResponsavel()+"%' ";	
+				ver++;
+			}
+		}
+		return sql;
+	}
+		
+		public List<Departamento> cons(Departamento d) throws SQLException {
+		List<Departamento> lista = new ArrayList<Departamento>();
+		PreparedStatement ps = c.prepareStatement(getSql(d));
 		ResultSet rs = ps.executeQuery();
 		while(rs.next()){
 			Departamento dp = new Departamento();
